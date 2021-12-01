85b7b90bd6101d0b3bd236277c5ee4cec6fca568
Jeff Haddock <jeff.haddock.developer@gmail.com>
2021-12-01 17:09:00 -0500

    removes iterator on uniq test

--- spec/part1.js
+++ spec/part1.js
@@ -328,13 +328,6 @@
         expect(_.uniq(numbers)).to.eql([1, 2, 3, 4]);
       });
 
-      it('should handle iterators that work with a sorted array', function() {
-        var iterator = function(value) { return value === 1; };
-        var numbers = [1, 2, 2, 3, 4, 4];
-
-        expect(_.uniq(FILL_ME_IN)).to.eql([1, 2]);
-      });
-
       it('should produce a brand new array instead of modifying the input array', function() {
         var numbers = [1, 2, 1, 3, 1, 4];
         var uniqueNumbers = _.uniq(numbers);
@@ -424,7 +417,7 @@
       it('should not mutate the input array', function() {
         var input = [1, 2, 3, 4, 5];
         var result = _.reduce(input, function(memo, item) { return item; });
-        
+
         /*
          * Mutation of inputs should be avoided without good justification otherwise
          * as it can often lead to hard to find bugs and confusing code!
